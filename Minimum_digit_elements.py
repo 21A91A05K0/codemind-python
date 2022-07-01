8eb76af6a111341e11fc2ac108b5a61ce9a63d17
21A91A05K0 <97424441+21A91A05K0@users.noreply.github.com>
2022-07-01 11:36:08 +0530

    n=int(input()) arr=list(map(int,input().split())) l=[] s=0 for i in range(n):     c=0     while(arr[i]):         c+=1         arr[i]//=10     l.append(c) for i in l:     if i==min(l):         s+=1 print(s)

---
+++ Minimum_digit_elements.py
@@ -0,0 +1,14 @@
+n=int(input())
+arr=list(map(int,input().split()))
+l=[]
+s=0
+for i in range(n):
+    c=0
+    while(arr[i]):
+        c+=1
+        arr[i]//=10
+    l.append(c)
+for i in l:
+    if i==min(l):
+        s+=1
+print(s)
